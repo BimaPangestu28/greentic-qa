5766ac79b6dc24c65fc8203d8119649d9fd680c2
thejobsmarket <152418647+thejobsmarket@users.noreply.github.com>
2026-01-26 09:13:56 +0000

    fix deploy

--- ci/scripts/smoke.sh
+++ ci/scripts/smoke.sh
@@ -10,13 +10,13 @@ mkdir -p "$SMOKE_OUTPUT_DIR"
 cargo run -p qa-cli -- --help
 cargo run -p qa-cli -- generate --input "$SMOKE_INPUT" --out "$SMOKE_OUTPUT_DIR"
 
-BUNDLE_NAME=$(python3 - <<'PY'
+BUNDLE_NAME=$(python3 - "$SMOKE_INPUT" <<'PY'
 import json, sys
 with open(sys.argv[1]) as fh:
     data = json.load(fh)
     print(data.get("dir_name") or "")
 PY
-  "$SMOKE_INPUT")
+)
 
 if [[ -z "$BUNDLE_NAME" ]]; then
   echo "Unable to read bundle name from $SMOKE_INPUT"
